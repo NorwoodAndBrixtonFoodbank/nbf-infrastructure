b49e36f2b7acdd10b942e9ca4b9faa2ddd576da9
Ricky Kawagishi <ricky.kawagishi@softwire.com>
2024-05-16 09:17:13 +0100

    NONE: Update account ID to use lambeth foodbank account

--- bin/infrastructure.ts
+++ bin/infrastructure.ts
@@ -3,8 +3,10 @@ import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { InfrastructureStack } from "../lib/infrastructure-stack";
 
+const lambethFoodbankAccountId = "471112736904"
+
 const app = new cdk.App();
 new InfrastructureStack(app, "InfrastructureStack", {
-  env: { account: "143404046955", region: "eu-west-2" },
+  env: { account: lambethFoodbankAccountId, region: "eu-west-2" },
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
